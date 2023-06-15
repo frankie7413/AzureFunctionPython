ab807ac56002c7371657d0f90e0a959bd5c529e5
Francisco Rivas <rocky@Franciscos-Laptop.local>
2023-06-14 21:51:12 -0700

    inital setup

---
+++ function_app.py
@@ -0,0 +1,26 @@
+import logging
+import azure.functions as func
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="create_feature_flag")
+def create_feature_flag(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+
+    return func.HttpResponse(
+        "This HTTP triggered function executed successfully."\
+        " Pass a name in the query string or in the request body for a personalized response.",
+        status_code=200
+    )

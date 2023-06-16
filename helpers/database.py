af41a42139b3171f83c7181bb649c5ef2c233b8e
Francisco Rivas <rocky@macbook>
2023-06-15 21:24:12 -0700

    simple database setup

---
+++ helpers/database.py
@@ -0,0 +1,26 @@
+import os
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_secret(secret):
+    secret_value =  os.environ.get(secret)
+
+    if secret_value is None:
+        logging.info("Did not find secret value: {}!", secret)
+        return None
+
+    return secret_value
+
+# dialect+driver://username:password@host:port/database
+def connection_string():
+    host = get_secret('HOST')
+    port = get_secret('PORT')
+    database = get_secret('DATABASE')
+    username = get_secret('USERNAME')
+    password = get_secret('PASSWORD')
+
+    temp = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}"
+    print(temp)
+    return temp

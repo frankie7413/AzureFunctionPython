757210de4acc1872ed22368b4715f925f90c91f2
frankie7413 <rocky@macbook>
2023-07-12 16:41:04 -0700

    merge fixes

--- helpers/database.py
+++ helpers/database.py
@@ -4,14 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-<<<<<<< HEAD
 
 def get_secret(secret):
     secret_value = os.environ.get(secret)
-=======
-def get_secret(secret):
-    secret_value =  os.environ.get(secret)
->>>>>>> 630b7b35db671e83425a33b1e6870e52827766e2
 
     if secret_value is None:
         logging.info("Did not find secret value: {}!", secret)
@@ -19,7 +14,6 @@ def get_secret(secret):
 
     return secret_value
 
-<<<<<<< HEAD
 
 # dialect+driver://username:password@host:port/database
 def connection_string():
@@ -28,15 +22,6 @@ def connection_string():
     database = get_secret("DATABASE")
     username = get_secret("USERNAME")
     password = get_secret("PASSWORD")
-=======
-# dialect+driver://username:password@host:port/database
-def connection_string():
-    host = get_secret('HOST')
-    port = get_secret('PORT')
-    database = get_secret('DATABASE')
-    username = get_secret('USERNAME')
-    password = get_secret('PASSWORD')
->>>>>>> 630b7b35db671e83425a33b1e6870e52827766e2
 
     temp = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}"
     print(temp)
